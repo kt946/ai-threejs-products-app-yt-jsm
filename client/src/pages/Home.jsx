f4b8485db3f1fa2e1472527fc131747f14293617
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-03-31 11:52:24 -0400

    updated Home page and added CustomButton component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+import { headContainerAnimation, headContentAnimation, headTextAnimation, slideAnimation } from '../config/motion';
+import { CustomButton } from '../components';
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const snap = useSnapshot(state);
 
-export default Home
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section
+          className="home"
+          {...slideAnimation('left')}
+        >
+          <motion.header {...slideAnimation('down')}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+
+          <motion.div
+            className="home-content"
+            {...headContainerAnimation}
+          >
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl:block hidden" /> DO IT.
+              </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Create you unique and exclusive shirt with our brand-new 3D customization tool.{' '}
+                <strong>Unleash your imagination</strong> and define your own style.
+              </p>
+
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => (state.intro = false)}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
