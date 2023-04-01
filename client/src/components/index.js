958819e77fd6ea0f22541a1943d351179178e292
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-04-01 12:53:52 -0400

    added additional components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,7 @@
 import CustomButton from './CustomButton';
+import AIPicker from './AIPicker';
+import ColorPicker from './ColorPicker';
+import FilePicker from './FilePicker';
+import Tab from './Tab';
 
-export { CustomButton };
+export { CustomButton, AIPicker, ColorPicker, FilePicker, Tab };
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import config from '../config/config';
+import state from '../store';
+import { download } from '../assets';
+import { downloadCanvasToImage, reader } from '../config/helpers';
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
+import { fadeAnimation, slideAnimation } from '../config/motion';
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 const Customizer = () => {
-  return (
-    <div>Customizer</div>
-  )
-}
+  return <AnimatePresence>Customizer</AnimatePresence>;
+};
 
-export default Customizer
+export default Customizer;
