d4353e488a3964f06cacff0ea457e41d8136030c
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-04-01 13:34:13 -0400

    updated CameraRig

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -19,7 +19,7 @@ const Customizer = () => {
         <>
           <motion.div
             key="custom"
-            className="absolute, top-0 left-0 z-10"
+            className="absolute top-0 left-0 z-10"
             {...slideAnimation('left')}
           >
             <div className="flex items-center min-h-screen">
