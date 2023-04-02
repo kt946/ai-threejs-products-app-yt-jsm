fa44f7618045ce2cfbecc559d9b5fbd74902b48d
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-04-02 13:11:10 -0400

    updated FilePicker component

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSnapshot } from 'valtio';
 
 import state from '../store';
+import { getContrastingColor } from '../config/helpers';
 
 const CustomButton = ({ type, title, customStyles, handleClick }) => {
   const snap = useSnapshot(state);
@@ -10,7 +11,13 @@ const CustomButton = ({ type, title, customStyles, handleClick }) => {
     if (type === 'filled') {
       return {
         backgroundColor: snap.color,
-        color: '#fff',
+        color: getContrastingColor(snap.color),
+      };
+    } else if (type === 'outline') {
+      return {
+        borderWidth: '1px',
+        borderColor: snap.color,
+        color: snap.color,
       };
     }
   };
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -30,12 +30,58 @@ const Customizer = () => {
       case 'colorpicker':
         return <ColorPicker />;
       case 'filepicker':
-        return <FilePicker />;
+        return (
+          <FilePicker
+            file={file}
+            setFile={setFile}
+            readFile={readFile}
+          />
+        );
       case 'aipicker':
         return <AIPicker />;
       default:
         return null;
     }
+  };
+
+  const handleDecals = (type, result) => {
+    const decalType = DecalTypes[type];
+
+    state[decalType.stateProperty] = result;
+
+    if (!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab);
+    }
+  };
+
+  const handleActiveFilterTab = (tabName) => {
+    switch (tabName) {
+      case 'logoShirt':
+        state.isLogoTexture = !activeFilterTab[tabName];
+        break;
+      case 'stylishShirt':
+        state.isFullTexture = !activeFilterTab[tabName];
+        break;
+      default:
+        state.isFullTexture = true;
+        state.isLogoTexture = false;
+        break;
+    }
+
+    // after setting the state, activeFilterTab is updated
+    setActiveFilterTab((prevState) => {
+      return {
+        ...prevState,
+        [tabName]: !prevState[tabName],
+      }
+    })
+  };
+
+  const readFile = (type) => {
+    reader(file).then((result) => {
+      handleDecals(type, result);
+      setActiveEditorTab('');
+    });
   };
 
   return (
@@ -83,8 +129,8 @@ const Customizer = () => {
                 key={tab.name}
                 tab={tab}
                 isFilterTab
-                isActiveTab=""
-                handleClick={() => {}}
+                isActiveTab={activeFilterTab[tab.name]}
+                handleClick={() => handleActiveFilterTab(tab.name)}
               />
             ))}
           </motion.div>
