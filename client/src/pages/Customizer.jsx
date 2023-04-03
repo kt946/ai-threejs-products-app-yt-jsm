4efa308a8a7163c750606535136bc4985c8875ba
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-04-03 14:43:12 -0400

    updated AIPicker function

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -55,7 +55,19 @@ const Customizer = () => {
     if (!prompt) return alert('Please enter a prompt');
 
     try {
-      // call our backend to generate an ai image!
+      setGeneratingImg(true);
+
+      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ prompt }),
+      }); // pass prompt to the backend api
+
+      const data = await response.json(); // get the response from the backend api
+
+      handleDecals(type, `data:image/png;base64,${data.photo}`); // handle the response
     } catch (error) {
       alert(error);
     } finally {
@@ -153,6 +165,13 @@ const Customizer = () => {
                 handleClick={() => handleActiveFilterTab(tab.name)}
               />
             ))}
+            <button className='download-btn' onClick={downloadCanvasToImage}>
+              <img
+                src={download}
+                alt='download_image'
+                className='w-3/5 h-3/5 object-contain'
+              />
+            </button>
           </motion.div>
         </>
       )}
